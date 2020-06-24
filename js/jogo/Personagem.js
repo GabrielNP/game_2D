6359cd2c05d8f156f8c5b53149246d041e47f663
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-23 23:28:59 -0300

    Adicionando inimigo, pulo, colisão e efeito sonoro do pulo

--- js/jogo/Personagem.js
+++ js/jogo/Personagem.js
@@ -1,49 +1,38 @@
-class Personagem {
+class Personagem extends Animacao {
 
-    constructor(imagem) {
-
-        this.imagem = imagem;
-        this.matriz = [
-            [0 ,0]
-            , [220, 0]
-            , [440, 0]
-            , [660, 0]
-            , [0, 270]
-            , [220, 270]
-            , [440, 270]
-            , [660, 270]
-            , [0, 540]
-            , [220, 540]
-            , [440, 540]
-            , [660, 540]
-            , [0, 810]
-            , [220, 810]
-            , [440, 810]
-            , [660, 810]
-        ];
-        this.frameAtual = 0;
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);        
+        this.yInicial = height - this.altura;
+        this.y = this.yInicial;
+        this.gravidade = 3;
+        this.velocidadeDoPulo = 0;
     }
 
-    exibe() {
+    pula() {
+        this.y -= 50;
+        this.velocidadeDoPulo = -30
+    }
 
-        image(this.imagem, 
-            0, 
-            height - 135, 
-            110, 
-            135, 
-            this.matriz[this.frameAtual][0], 
-            this.matriz[this.frameAtual][1], 
-            200, 
-            270
+    aplicaGravidade() {
+        this.y += this.velocidadeDoPulo;
+        this.velocidadeDoPulo += this.gravidade;
+
+        if (this.y > this.yInicial) this.y = this.yInicial;
+    }
+
+    estaColidindo(inimigo) {
+        const precisao = .7;
+        const colisao = collideRectRect(
+            this.x,
+            this.y,
+            this.largura * precisao,
+            this.altura * precisao,
+            inimigo.x,
+            inimigo.y,
+            inimigo.largura * precisao,
+            inimigo.altura * precisao
         );
-        
-        this.anima();
-    }
 
-    anima() {
-        
-        this.frameAtual++;
-
-        if (this.frameAtual >= this.matriz.length -1) { this.frameAtual = 0; }
+        return colisao;
     }
 }

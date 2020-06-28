1d95edc17106f19b67cab4bd8cf5ce63fb6386d1
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-28 20:34:29 -0300

    Reestruturando pastas do projeto e adicionando fonte Pangolin

--- sketch.js
+++ sketch.js
@@ -117,17 +117,17 @@ const inimigos = [];
 
 function preload() {
     
-    imagemCenario = loadImage('imagens/cenario/floresta.png');
-    imagemGameOver = loadImage('imagens/assets/game-over.png');
-    imagemPersonagem = loadImage('imagens/personagem/correndo.png');
-    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
-    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
-    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png')
+    imagemCenario = loadImage('assets/cenario/floresta.png');
+    imagemGameOver = loadImage('assets/game-over.png');
+    imagemPersonagem = loadImage('assets/personagem/correndo.png');
+    imagemInimigo = loadImage('assets/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('assets/inimigos/troll.png');
+    imagemInimigoVoador = loadImage('assets/inimigos/gotinha-voadora.png')
 
     somDoJogo = loadSound('sons/trilha_jogo.mp3');
     somDoPulo = loadSound('sons/somPulo.mp3');
 
-    pangolin = loadFont('imagens/assets/Pangolin/Pangolin-Regular.ttf');
+    pangolin = loadFont('assets/fonts/Pangolin/Pangolin-Regular.ttf');
 }
 
 function setup() {
